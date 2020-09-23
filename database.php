c46aa76ba0455682cf29da4a7fc24f91b6b5d6dd
NBadeloe <nandita.badeloe@student.rocva.nl>
2020-09-23 23:46:14 +0200

    added db connection, execute function needs debugging

---
+++ database.php
@@ -0,0 +1,51 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fname = $_POST['fname'];
+    $insertion = $_POST['insertion'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $usrname = $_POST['username'];
+    $passwrd = $_POST['password'];
+    $hash = md5($passwrd);
+    }  
+
+
+class Database
+{
+    private $host;
+    private $user;
+    private $password;  
+    private $database;
+    private $charset;
+    private $db;
+
+ 
+
+    public function __construct($host, $user, $password, $database, $charset)
+    {
+
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->database = $database;
+        $this->charset = $charset;
+
+        try {
+            $dsn = "mysql:host=localhost;$this->host;dbname=$this->database;charset=$this->charset";
+            $this->db = new PDO($dsn, $this->user, $this->password);
+            echo "connected!";
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            exit("connection failed");
+        }
+    }
+
+    public function executeQuery($fname, $insertion, $lname, $email, $usrname, $hash){
+        $sql = "INSERT INTO project1 (first_name, insertion, last_name, email, username, password) VALUES ($fname, $insertion, $lname, $email, $usrname, $hash) ";
+        $statement = $this->db->prepare($sql);
+        $statement->execute($sql);        
+    }
+}
+
+?>
--- signup.php
+++ signup.php
@@ -1,8 +1,24 @@
+<?php
+
+include 'database.php';
+// $_SERVER["REQUEST_METHOD"] == "POST";
+// $fname = $_POST['fname'];
+// $insertion = $_POST['insertion'];
+// $lname = $_POST['lname'];
+// $email = $_POST['email'];
+// $usrname = $_POST['username'];
+// $passwrd = $_POST['password'];
+// $hash = md5($passwrd);
+
+$db = new Database('localhost', 'root', '', 'project1', 'utf8');
+$db->executeQuery($fname, $insertion, $lname, $email, $usrname, $hash);
+
+?>
 <html>
   <body>
     <h2>sign up</h2>
 
-    <form action="signup.php" method="POST">
+    <form action="database.php" method="POST">
       <input type="text" id="fname" name="fname" required placeholder="First name"><br>
 
       <input type="text" id="insertion" name="insertion" placeholder="Insertion"><br> 
@@ -15,7 +31,7 @@
 
       <input type="password" id="password" name="password" required placeholder="password"><br>
 
-      <input type="repassword" id="repassword" name="repassword" required placeholder="Repeat password"><br>
+      <input type="password" id="repassword" name="repassword" required placeholder="Repeat password"><br>
 
       <button type="submit">sign up</button>
 
